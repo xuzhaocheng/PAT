b9264003235a8d7b1f484881ebe24a2baac665ff
xuzhaocheng <zhaochengxu1991@gmail.com>
2015-03-09 21:41:26 +0800

    add 1063

--- src/1063.Set Similarity.cpp
+++ src/1063.Set Similarity.cpp
@@ -1,3 +1,7 @@
+/*
+** 考察set的使用
+** unordered_map超时
+*/
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,7 +14,6 @@
 
 using namespace std;
 
-
 void calcSetSimilarity(set<string> &x, set<string> &y) {
 	int size = max(x.size(), y.size());
 	vector<string> v(size);
@@ -20,14 +23,13 @@ void calcSetSimilarity(set<string> &x, set<string> &y) {
 	
 	int intersection_size = it - v.begin();
 	// 并集个数等于相加减交集个数
+	
 	double total = x.size() + y.size() - intersection_size;
-
-	printf("%.1f", intersection_size / total * 100);
-	cout << "%" << endl;
+	printf("%.1f%%\n", intersection_size / total * 100);
 }
 
 int main(void) {
-	// freopen("../in/1063.in", "r", stdin);
+	freopen("../in/1063.in", "r", stdin);
 	
 	int n;
 	cin >> n;
@@ -55,52 +57,3 @@ int main(void) {
 
 	return 0;
 }
-
-/*
-void calcSetSimilarity(unordered_map<string, int> &x, const unordered_map<string, int> &y) {
-	unordered_map<string, int> union_set;
-	unordered_map<string, int>::iterator it;
-
-	union_set = y;
-	int cnt = 0;
-	for (it = x.begin(); it != x.end(); it++) {
-		if (union_set.find(it->first) != union_set.end())
-			cnt++;
-		else 
-			union_set[it->first] = it->second;
-	}
-
-	double res = cnt / (double)union_set.size() * 100;
-	printf("%.1f", res);
-	cout << "%\n";
-}
-
-int main(void) {
-	// freopen("../in/1063.in", "r", stdin);
-	
-	int n;
-	cin >> n;
-
-	vector<unordered_map<string, int> > sets(n);
-	int m;
-	for (int i = 0; i < n; i++) {
-		cin >> m;
-		unordered_map<string, int> set;
-		string str_num;
-		for (int j = 0; j < m; j++) {
-			cin >> str_num;
-			set[str_num]++;
-		}
-		sets[i] = set;
-	}
-
-	int k;
-	cin >> k;
-	int idx1, idx2;
-	for (int i = 0; i < k; i++) {
-		cin >> idx1 >> idx2;
-		calcSetSimilarity(sets[idx1 - 1], sets[idx2 - 1]);
-	}
-
-	return 0;
-}*/
